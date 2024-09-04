2b87e22aff4d523723af90928d31efb2625d15fa
ibragimBack <aibarchyn61@gmail.com>
2024-09-04 18:52:14 +0600

    автоматическая скидка

--- calculator/views.py
+++ calculator/views.py
@@ -2,6 +2,21 @@ from django.shortcuts import render
 from . forms import CalculationForm
 from django.http import JsonResponse
 
+def calculate_discount(total_amount):
+    if 1000 <= total_amount < 10000:
+        return 5
+    elif 10000 <= total_amount < 20000:
+        return 10
+    elif 20000 <= total_amount < 30000:
+        return 15
+    elif 30000 <= total_amount <40000:
+        return 17
+    elif 40000 <= total_amount < 50000:
+        return 20
+    
+    return 0
+    
+
 def calculator_view(request):
     if request.method == 'POST':
         form = CalculationForm(request.POST)
@@ -10,9 +25,10 @@ def calculator_view(request):
             quadrature = form.cleaned_data['quadrature']
             installation = form.cleaned_data['installation']
             payment = form.cleaned_data['payment']
-            discount = form.cleaned_data['discount']
 
-            total_amount = fabric.price * quadrature + installation
+            fabric_price = fabric.price if fabric else 0
+            total_amount = fabric_price * quadrature + installation
+            discount = calculate_discount(total_amount)
             total_with_discount = total_amount * ((100 - discount) / 100)
             final_amount = total_with_discount * ((100 + payment.percentage) / 100)
             
@@ -21,6 +37,7 @@ def calculator_view(request):
                 'total_amount': total_amount,
                 'total_with_discount': total_with_discount,
                 'final_amount': final_amount,
+                'discount': discount,
             }
             return JsonResponse(data)
         
